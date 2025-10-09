cfa6580fe58046d49f4fb25e35b0b6d44f2c24ed
Roger BC <roger@chaindots.com>
2025-10-08 22:18:48 -0300

    feat: implementar navegación en la pantalla de inicio y agregar pantallas de búsqueda y plan

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,86 @@
 import 'package:flutter/material.dart';
-import 'package:que_comer_hoy/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:que_comer_hoy/screens/profile_screen.dart';
+import 'package:que_comer_hoy/screens/search_screen.dart';
+import 'package:que_comer_hoy/screens/plan_screen.dart';
+import 'package:que_comer_hoy/services/auth_service.dart';
 
-class HomeScreen extends StatelessWidget {
-  final AuthService _auth = AuthService();
+class HomeScreen extends StatefulWidget {
   final User user;
 
-  HomeScreen({super.key, required this.user});
+  const HomeScreen({Key? key, required this.user}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  late final List<Widget> _widgetOptions;
+
+  @override
+  void initState() {
+    super.initState();
+    _widgetOptions = <Widget>[
+      HomeTab(user: widget.user),
+      const SearchScreen(),
+      const PlanScreen(),
+      const ProfileScreen(),
+    ];
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Buscar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt),
+            label: 'Plan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Colors.grey,
+      ),
+    );
+  }
+}
+
+// This widget contains the original content of the HomeScreen
+class HomeTab extends StatelessWidget {
+  final User user;
+  final AuthService _auth = AuthService();
+
+  HomeTab({Key? key, required this.user}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('Inicio'),
         actions: [
           IconButton(
             icon: const Icon(Icons.logout),
---
+++ lib/screens/plan_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PlanScreen extends StatelessWidget {
+  const PlanScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plan'),
+      ),
+      body: const Center(
+        child: Text('Plan Screen'),
+      ),
+    );
+  }
+}
